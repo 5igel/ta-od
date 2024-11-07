bbd544d201db40c31578086d20465a3831087c16
Sergii Kocherev <st.ixidor@gmail.com>
2024-11-07 12:08:02 +0100

    added reverse-proxy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,24 @@ import mkcert from 'vite-plugin-mkcert'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
+  server: {
+    proxy: {
+      "/api": {
+        target: 'https://swapi.dev/',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    cors: false,
+  },
+  preview: {
+    proxy: {
+      "/api": {
+        target: 'https://swapi.dev/',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    cors: false,
+  },
 })
